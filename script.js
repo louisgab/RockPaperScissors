847fb1ce3e5f784a28559858c94347252cb75a1d
Louis-Gabriel <louisgab@users.noreply.github.com>
2017-02-08 12:27:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,27 +3,27 @@
  * https://github.com/louisgab/RockPaperScissors.git
  */
 var humanScore = computerScore = 0,
-    choices = ["pierre", "feuille", "ciseaux"],
+    names = ["Rock", "Paper", "Scissors"],
     symbols = ["✊", "✋", "✌"];
 
 function showScores(){
-    document.getElementById("human-score").innerHTML = "Vous : <b>"+humanScore+"</b>";
-    document.getElementById("computer-score").innerHTML = "Ordinateur : <b>"+computerScore+"</b>";
+    document.getElementById("human-score").innerHTML = "You : <b>"+humanScore+"</b>";
+    document.getElementById("computer-score").innerHTML = "Computer : <b>"+computerScore+"</b>";
 }
 
 function main(humanChoice){
     var computerChoice = Math.floor((Math.random() * 2)),
     message = "";
-    document.getElementById("human-choice").innerHTML = symbols[humanChoice];
-    document.getElementById("computer-choice").innerHTML = symbols[computerChoice];
+    document.getElementById("human-choice").innerHTML = symbols[humanChoice]+"<br>"+names[humanChoice];
+    document.getElementById("computer-choice").innerHTML = symbols[computerChoice]+"<br>"+names[computerChoice];
     if (humanChoice === computerChoice)
-        message = "😫 Egalité !";
+        message = "😫 Tie !";
     else if ((humanChoice === 0 && computerChoice === 2) || (humanChoice === 1 && computerChoice === 0) || (humanChoice === 2 && computerChoice === 1)){
-        message = "😎 Gagné !";
+        message = "😎 Win !";
         humanScore++;
     }
     else{
-        message = "😥 Perdu !";
+        message = "😥 Lost !";
         computerScore++;
     }
     document.getElementById("message").innerHTML = message;
